aa302a802dfec8012037fdc0a5c9904d25e2e3e8
rodrige <peniouatt@hotmail.fr>
2019-07-11 15:56:15 +0200

    implementation de l'algo de selection du type de mutation

--- GeneticTerrain/GeneticTerrain.cs
+++ GeneticTerrain/GeneticTerrain.cs
@@ -77,8 +77,8 @@ namespace GeneticTerrain
         /// </summary>
         /// <param name="p"></param>
         /// <returns></returns>
-        public List<(Algorithm,Algorithm)> Meetic(List<Algorithm> p)
-        {  
+        public List<(Algorithm, Algorithm)> Meetic(List<Algorithm> p)
+        {
             var AlgorithmCouples = new List<(Algorithm, Algorithm)>();
             var random = new Random();
             foreach (var elmt in p)
@@ -87,11 +87,90 @@ namespace GeneticTerrain
                 {
                     int index = random.Next(p.Count);
                     AlgorithmCouples.Add((elmt, p[index]));
-                    if(AlgorithmCouples.Count==maxPopulation) return AlgorithmCouples;
+                    if (AlgorithmCouples.Count == maxPopulation) return AlgorithmCouples;
                 }
             }
             return AlgorithmCouples;
         }
+
+
+    
+        private (Algorithm, Algorithm) FirstElmtInsideSecondElmtSuffle((Algorithm a1, Algorithm a2) algo)
+        {
+            Algorithm a1, a2;
+            a1 = null;
+            a2 = null;
+            // reste à implemewnter avec le mutationVisitor
+            var newCouple = (a1,a2);
+            return newCouple;
+        }
+
+        private (Algorithm, Algorithm) PermuteFirstElmtOneWithSecondElmtSuffle((Algorithm a1, Algorithm a2) algo)
+        {
+            Algorithm a1, a2;
+
+            a1 = null;
+            a2 = null;
+            // reste à implemewnter avec le mutationVisitor 
+            var newCouple = (a1, a2);
+            return newCouple;
+        }
+
+        private List<Algorithm> RandomlyAplySuffle(List<(Algorithm a1, Algorithm a2)> algoCouples)
+        {
+            var suffleGroup1 = new List<(Algorithm,Algorithm)>();
+            var suffleGroup2 = new List<(Algorithm,Algorithm)>();
+            var newAlgoCouples = new List<(Algorithm, Algorithm)>();
+            var totalAlgoList = new List<Algorithm>();
+            var random = new Random();
+            
+            // list 1
+            for(int i = 0; i<= algoCouples.Count/2; i++)
+            {
+                int index = random.Next(algoCouples.Count);
+                foreach(var elmt in algoCouples )
+                {
+                    suffleGroup1.Add(elmt);
+                }
+                
+            }
+
+            // list 2
+            foreach(var elmt in algoCouples)
+            {
+                foreach(var el in suffleGroup1)
+                {
+                    if (elmt.ToString() != el.ToString())
+                    {
+                        suffleGroup2.Add(elmt);
+                    }
+                }
+            }
+
+            // j'applique le suffle 1 sur la list 1
+            foreach(var elmt in suffleGroup1)
+            {
+                var result = FirstElmtInsideSecondElmtSuffle(elmt);
+                newAlgoCouples.Add(result);
+            }
+
+            // j'applique le suffle 2 sur list 2
+            foreach(var elmt in suffleGroup1)
+            {
+                var result = PermuteFirstElmtOneWithSecondElmtSuffle(elmt);
+                newAlgoCouples.Add(result);
+            }
+
+            //je sépare le tuple et j'ajoute à la liste
+            foreach(var elmt in newAlgoCouples)
+            {
+                totalAlgoList.Add(elmt.Item1);
+                totalAlgoList.Add(elmt.Item2);
+            }
+
+            return totalAlgoList;
+        }
+      
 
         public Algorithm runSimulation()
         {
